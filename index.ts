dbc079be682d322bccd6e7fd1a448e92e7e30eed
LeshaPI <pialeksej5@gmail.com>
2023-01-23 23:23:59 +0300

    fix: env port

--- index.ts
+++ index.ts
@@ -7,7 +7,8 @@ const HTTP_PORT = 8181;
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
 
-const WS_PORT = 8080;
+const PORT = Number(process.env.WS_PORT);
+const WS_PORT = PORT || 8080;
 const ws = new WebSocketServer({port: WS_PORT}, () => {console.log(`Start WS server on the ${WS_PORT} port!`)});
 
 ws.on('connection', async (ws) => await commandsHandler( ws ));
